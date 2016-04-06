61cb29707750efe697aa405dd3fde3177f525946
Denys Kashyrin <desya81@gmail.com>
2016-04-06 15:07:41 +0300

    home work for Module 1 Enterprise

--- src/module1ForEnterprise/ObjectService.java
+++ src/module1ForEnterprise/ObjectService.java
@@ -18,7 +18,13 @@ public class ObjectService {
         this.countOfData = countOfData;
     }
 
-    public void showResultTable() throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, FileNotFoundException, UnsupportedEncodingException {
+    public void showResultTable() throws ClassNotFoundException,
+            NoSuchMethodException,
+            InvocationTargetException,
+            IllegalAccessException,
+            FileNotFoundException,
+            UnsupportedEncodingException {
+
         PrintWriter writer = new PrintWriter("the-file-name.txt", "UTF-8");
         HashMap<String, HashMap<String, Long>> result = this.getCalculationResult();
 
@@ -43,72 +49,136 @@ public class ObjectService {
                     containsF = entry2.getValue();
                 }else if(entry2.getKey().equals("populate")){
                     populateF = entry2.getValue();
-                }else if(entry2.getKey().equals("iteratorAddF")){
+                }else if(entry2.getKey().equals("iterator.add")){
                     iteratorAddF = entry2.getValue();
-                }else if(entry2.getKey().equals("iteratorRemoveF")){
+                }else if(entry2.getKey().equals("iterator.remove")){
                     iteratorRemoveF = entry2.getValue();
                 }
             }
-            System.out.printf("%-11s %-7s %-9s %-7s %-7s %-7s %-15s %-7s%n", entry.getKey(), addF, getF, removeF, containsF, populateF, iteratorAddF, iteratorRemoveF);
-            writer.printf("%-11s %-7s %-9s %-7s %-7s %-7s %-15s %-7s%n", entry.getKey(), addF, getF, removeF, containsF, populateF, iteratorAddF, iteratorRemoveF);
+            System.out.printf("%-11s %-7s %-9s %-7s %-7s %-10s %-15s %-7s%n", entry.getKey(), addF, getF, removeF, containsF, populateF, iteratorAddF, iteratorRemoveF);
+            writer.printf("%-11s %-7s %-9s %-7s %-7s %-10s %-15s %-7s%n", entry.getKey(), addF, getF, removeF, containsF, populateF, iteratorAddF, iteratorRemoveF);
         }
         writer.close();
     }
 
     private HashMap<String, HashMap<String, Long>> getCalculationResult() {
         HashMap<String, HashMap<String, Long>> result = new HashMap<String, HashMap<String, Long>>();
-        for (int k = 0; k<this.objectsForTest.length; k++){
-            String type = this.objectsForTest[k];
-            HashMap<String, Long> tmpData = new HashMap<String, Long>();
-            Long averageTime = null;
-            ArrayList<Integer> obj = new ArrayList<Integer>();
-            for (int i = 0; i < this.functionsForObjects.length; i++) {
-                if(this.functionsForObjects[i].equals("add")){
-                    long startTime = System.currentTimeMillis();
-                    for (int j = 0; j < this.countOfData; j++) {
-                        obj.add(j);
-                    }
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }else if(this.functionsForObjects[i].equals("get")){
-                    long startTime = System.currentTimeMillis();
-                    for (int j = 0; j < this.countOfData; j++) {
-                        obj.get(j);
-                    }
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }else if(this.functionsForObjects[i].equals("remove")){
-                    long startTime = System.currentTimeMillis();
-                    for (int j = 0; j < obj.size(); j++) {
-                        obj.remove(j);
-                    }
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }else if(this.functionsForObjects[i].equals("contains")){
-                    long startTime = System.currentTimeMillis();
-                    obj.contains(countOfData);
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }else if(this.functionsForObjects[i].equals("populate")){
-                    long startTime = System.currentTimeMillis();
-                    // TODO implement logic
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }else if(this.functionsForObjects[i].equals("iterator.add")){
-                    long startTime = System.currentTimeMillis();
-                    // TODO implement logic
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }else if(this.functionsForObjects[i].equals("iterator.remove")){
-                    long startTime = System.currentTimeMillis();
-                    // TODO implement logic
-                    long endTime = System.currentTimeMillis();
-                    averageTime = (endTime - startTime);
-                }
-                tmpData.put(functionsForObjects[i], averageTime);
-            }
-            result.put(type, tmpData);
-        }
+
+        ArrayList arrayList = new ArrayList<Integer>();
+        result.put("ArrayList", doListFunction(arrayList));
+        LinkedList linkedList = new LinkedList();
+        result.put("LinkedList", doListFunction(linkedList));
+        Set hashSet = new HashSet();
+        result.put("HashSet", doSetFunction(hashSet));
+        Set treeSet = new TreeSet();
+        result.put("TreeSet", doSetFunction(treeSet));
+
         return result;
+    }
+
+    /**
+     * Function List function
+     * @param obj List
+     * @return HashMap
+     */
+    private HashMap doListFunction(List obj){
+        HashMap<String, Long> tmpData = new HashMap<String, Long>();
+        Random rand = new Random();
+        long startTime;
+        long endTime;
+        long averageTime;
+
+        startTime = System.currentTimeMillis();
+        for (int j = 0; j < this.countOfData; j++) {
+            obj.add(j);
+        }
+        endTime = System.currentTimeMillis();
+        long averagePopulateTime = (endTime - startTime);
+
+        for (int i = 0; i < this.functionsForObjects.length; i++) {
+            startTime = System.currentTimeMillis();
+            if(this.functionsForObjects[i].equals("add")){
+                for (int j = 0; j < 100; j++) {
+                    obj.add(j);
+                }
+            }else if(this.functionsForObjects[i].equals("get")){
+                for (int j = 0; j < 100; j++) {
+                    obj.get(j);
+                }
+            }else if(this.functionsForObjects[i].equals("remove")){
+
+                for (int j = 0; j < 100; j++) {
+                    obj.remove(j);
+                }
+            }else if(this.functionsForObjects[i].equals("contains")){
+                obj.contains(100);
+            }else if(this.functionsForObjects[i].equals("iterator.add")){
+                ListIterator ArrayIterator = obj.listIterator();
+                for (int j = 0; j < 100; j++) {
+                    ArrayIterator.add(rand.nextInt());
+                }
+            }else if(this.functionsForObjects[i].equals("iterator.remove")){
+                ListIterator ArrayIterator = obj.listIterator();
+                for (int j = 0; j < 100; j++) {
+                    ArrayIterator.next();
+                    ArrayIterator.remove();
+                }
+            }
+            endTime = System.currentTimeMillis();
+            if(this.functionsForObjects[i].equals("populate")){
+                averageTime = averagePopulateTime;
+            }else{
+                averageTime = (endTime - startTime);
+            }
+            tmpData.put(functionsForObjects[i], averageTime);
+        }
+
+        return tmpData;
+    }
+
+
+    private HashMap doSetFunction(Set obj){
+        HashMap<String, Long> tmpData = new HashMap<String, Long>();
+        Random rand = new Random();
+        long startTime;
+        long endTime;
+        long averageTime;
+
+        startTime = System.currentTimeMillis();
+        for (int j = 0; j < this.countOfData; j++) {
+            obj.add(j);
+        }
+        endTime = System.currentTimeMillis();
+        long averagePopulateTime = (endTime - startTime);
+
+        for (int i = 0; i < this.functionsForObjects.length; i++) {
+            startTime = System.currentTimeMillis();
+            if(this.functionsForObjects[i].equals("add")){
+                for (int j = 0; j < 100; j++) {
+                    obj.add(j);
+                }
+            }else if(this.functionsForObjects[i].equals("remove")){
+                for (int j = 0; j < 100; j++) {
+                    obj.remove(j);
+                }
+            }else if(this.functionsForObjects[i].equals("contains")){
+                obj.contains(100);
+            }else if(this.functionsForObjects[i].equals("iterator.remove")){
+                Iterator iterator = obj.iterator();
+                for (int j = 0; j < 100; j++) {
+                    iterator.next();
+                    iterator.remove();
+                }
+            }
+            endTime = System.currentTimeMillis();
+            if(this.functionsForObjects[i].equals("populate")){
+                averageTime = averagePopulateTime;
+            }else{
+                averageTime = (endTime - startTime);
+            }
+            tmpData.put(functionsForObjects[i], averageTime);
+        }
+
+        return tmpData;
     }
 }
